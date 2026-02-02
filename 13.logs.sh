95dc02e243a40363135dd4f7ec899304f83f5abc
Gowrish <gowrish.gu@gmail.com>
2026-02-02 14:58:48 +0530

    actions

--- 13.logs.sh
+++ 13.logs.sh
@@ -1,41 +1,30 @@
 #!/bin/bash
 
-
-USERID=$(id -u)  ##User ID --> Root user always 0
+USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
-if [[ $USERID -ne 0 ]]; then
-    echo "Run this script with root user access"
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
     exit 1
 fi
 
 mkdir -p $LOGS_FOLDER
 
-func_install(){
-    if [[ $1 -ne 0 ]]; then
-        echo "$2 ...Installing failed"
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+        exit 1
     else
-        echo "$2 ...Installing  success"
-fi
+        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
+    fi
 }
 
-
 dnf install nginx -y &>> $LOGS_FILE
-func_install $? "Installing Nginx"
+VALIDATE $? "Installing Nginx"
 
 dnf install mysql -y &>> $LOGS_FILE
-func_install $? "Installing mysql"
+VALIDATE $? "Installing Mysql"
 
 dnf install nodejs -y &>> $LOGS_FILE
-func_install $? "Installing nodejs"
-
-
-
-
-
-
-
-
-
-
+VALIDATE $? "Installing nodejs"
