bf5ecfe269aa9392a1dbf32dce4dececb26d808a
Gowrish <gowrish.gu@gmail.com>
2026-02-02 13:19:43 +0530

    actions

--- 11.functions.sh
+++ 11.functions.sh
@@ -3,7 +3,12 @@
 my_function(){
     echo "hell from function"
 }
-
-###itself no output when we call it
-
+###itself no output when we call it, then only output
 my_function
+
+
+install_nginx() {
+    dnf install nginx -y
+}
+
+install_nginx
