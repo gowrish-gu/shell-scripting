7a68f31de7f8da1bf050612cdb19615d24c1a401
Gowrish <gowrish.gu@gmail.com>
2026-07-03 18:19:45 +0530

    actions

---
+++ 09-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+Number=25
+
+# -gt greater than
+# -lt less than
+# -eq equals to
+# -ne not euals to
+
+if [ $Number -gt 30 ]; then
+    echo "Greater than 25"
+
+fi
+    echo "Less than 25"
