fd4dd8dd8d1f30d301b9b375c70e532b884f69cb
Gowrish <gowrish.gu@gmail.com>
2026-07-03 17:44:36 +0530

    actions

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -3,10 +3,14 @@
 ### Bash treats these values as strings by default ###
 
 #NUM1=100
-NUM1=Gowrish
+#NUM1=Gowrish
 NUM2=200
 
 SUM=$(($NUM1+$NUM2))
 
 echo "Sum is : $SUM"
 
+### Array ####
+
+Fruits=("Apple" "Bananna" "Oranage")
+echo "Fruits are : ${Fruits[@]}"
