58f863b2e1cec5d87b82a7a7e9b8601f91d55fdf
Gowrish <gowrish.gu@gmail.com>
2026-07-03 16:56:53 +0530

    actions

--- 06-variable.sh
+++ 06-variable.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+START_TIME=$(date +%s)
 #$(date) output of the command stored in the variable timestamp
-echo "Script executed at: $TIMESTAMP"
+echo "Script executed at: $START_TIME"
+
+sleep 10
+
+echo 
+END_TIME=$(date +%s)
+echo "Script executed at: $END_TIME"
+
+TOTAL=$START_TIME-$END_TIME
+
+echo "Total time script ececuted is: $TOTAL"
+
+
+
