d2fc3ac8aa5c374f40f31ef3792b13b93d1655ff
Gowrish <gowrish.gu@gmail.com>
2026-07-03 20:08:31 +0530

    actions

--- 10-nginx-install.sh
+++ 10-nginx-install.sh
@@ -2,11 +2,11 @@
 
 USER_ID=$(id -u)
 
-if [ $USER_ID -eq 0]; then
+if [ $USER_ID -eq 0 ]; then
     echo "Installing Nginx"
     dnf install nginx -y
 else
-    sudo "Please switch to root user or add sudo before running the script"
+    sudo "Please switch to root user or run the script with sudo"
     exit 1
 fi
 
