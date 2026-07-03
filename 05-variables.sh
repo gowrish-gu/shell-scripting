7c6500e2a8fdefd66215a15f029df8052bf05848
Gowrish <gowrish.gu@gmail.com>
2026-07-03 16:35:32 +0530

    commit

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,4 +2,14 @@
 
 # Dynamic Variables
 
-echo "Please enter your name"
+echo "Please enter your user name"
+echo "enter your gmail-id"
+
+read USER_NAME
+read GMAIl_ID
+
+echo "your username is $USER_NAME"
+echo "your gmail-id is $GMAIL_ID"
+
+echo Thank you...!
+
