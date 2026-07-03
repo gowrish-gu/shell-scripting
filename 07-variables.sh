1365ac36064e5bb3adbf43f7fd9acb65489b8f5a
Gowrish <gowrish.gu@gmail.com>
2026-07-03 17:17:42 +0530

    actions

---
+++ 07-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+### Special Variables ###
+
+#echo "Arguments are passed to script : $0"
+echo "All arguments passed to script : $@"
+echo "Number of arguments passed to script : $#"
+echo "Script name : $0"
+echo "Present directory : $PWD"
+echo "Who is running: $USER"
+echo "Home directory of current user: $HOME"
+echo "PID is : $$"
+sleep 100 &
+echo "PID of recently executed backdround process: $!"
+echo "All arguments to script : $*"
